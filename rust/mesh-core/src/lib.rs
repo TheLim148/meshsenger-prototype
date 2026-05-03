5524441ec13f28dc7bb72b5671b97c9cecd4b8ff
TheLim148 <ruzhov34@gmail.com>
2026-05-03 20:54:29 +0300

    Add models and dependencies

--- rust/mesh-core/src/lib.rs
+++ rust/mesh-core/src/lib.rs
@@ -1,3 +1,41 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct NodeId(pub String);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct MessageId(pub Uuid);
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ChatType {
+    Private,
+    Broadcast,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum MessageType {
+    Text,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Payload {
+    Text { text: String },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Message {
+    pub protocol_version: String,
+    pub message_id: MessageId,
+    pub message_type: MessageType,
+    pub chat_type: ChatType,
+    pub from: NodeId,
+    pub to: Option<NodeId>,
+    pub ttl: u8,
+    pub timestamp: u64,
+    pub payload: Payload,
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
